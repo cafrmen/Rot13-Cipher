adc1ad8ccb53cd89d3949d101ef10fdd230e969e
cafrmen <cafrmen@gmail.com>
2023-03-22 20:23:31 -0600

    Rot13 Cipher
    Portfolio project using React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [input, setInput] = useState('');
+    const [result, setResult] = useState('ZL CEBWRPG SEBZ SERRPBQRPNZC WF NYTBEVGUZF NAQ QNGN FGEHPGHERF');
+
+    const handleChange = (e) => setInput(e.target.value);
+
+    /* This is the JS magic for the Rot13 Cipher */
+    const handleSubmit = (e) => {
+      e.preventDefault();
+
+      var numStr = input.toUpperCase()
+                          .split('')
+                          .map(letter => letter.charCodeAt(0));
+
+      var letterStr = numStr.map(num => (num >= 65 && num <= 77) ?
+                                  num + 13 :
+                                  (num >= 78 && num <= 90) ?
+                                  num - 13 :
+                                  num);
+
+      var finalStr = letterStr.map(index => String.fromCharCode(index))
+                                .join('');
+
+      setResult(finalStr);
+    }
+
+    return (
+        <div className='App' >
+          <div className='contenedor-rot' >
+            <h1>Rot-13</h1>
+            <h3>
+              An encryption algorithm used in the early 1980s.
+            </h3>
+            <p>
+              Originated on Usenet, provides no real cryptographic security. It is often used as the canonical example of weak encryption. The algorithm is used in online forums as a means to hide information.
+            </p>
+
+            <form onSubmit={handleSubmit}>
+              <input className='cipher'
+                    type='text'
+                    name='cipher'
+                    placeholder='...'
+                    value={input}
+                    onChange={handleChange} />
+              <button type="submit" className='magic' >
+                Magic
+              </button>
+            </form>
+            <hr />
+            <div>
+              <p className='result'>
+                {result}
+              </p>
+            </div>
+          </div>
+        </div>
+    )
 }
 
 export default App;
